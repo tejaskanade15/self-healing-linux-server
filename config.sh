7630d9b8f8410e83bb3280a22ae7673a98897e98
Tejas Kanade <tejaskanade15@gmail.com>
2026-06-24 22:25:03 +0530

    Enhance Telegram and email notification system

--- config.sh
+++ config.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 
+###############################
+# SelfHeal Linux Configuration
+###############################
+
+# Alert Threshold
 THRESHOLD=80
-EMAIL="$EMAIL"
+
+# Email Recipient
+EMAIL="tejaskanade80@gmail.com"
+
+# Telegram Bot
+TOKEN="8672704932:AAHtECf7TpwQDUruiHgad9r3_bcjuA5BqN8"
+CHAT_ID="1470013672"
+
+# Host Information
 HOST=$(hostname)
-DATE=$(date)
+
+# Log File
 LOGFILE="/var/log/self_healing.log"
 
-TOKEN="$TELEGRAM_TOKEN"
-CHAT_ID="$CHAT_ID"
+#########################################
+# Function : Write to log file
+#########################################
 
 log_message() {
-echo "$(date) -$1">> $LOGFILE
+echo "[$(date '+%d-%b-%Y %I:%M:%S %p')] $1" >> "$LOGFILE"
 }
 
+#########################################
+# Function : Telegram Notification
+#########################################
+
 send_telegram() {
-MESSAGE=$1
-curl -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
--d "chat_id=$CHAT_ID" \
--d "text=$MESSAGE"
+
+MESSAGE="$1"
+
+curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" \
+-d chat_id="$CHAT_ID" \
+-d parse_mode="Markdown" \
+-d text="$MESSAGE" >/dev/null
+
 }
